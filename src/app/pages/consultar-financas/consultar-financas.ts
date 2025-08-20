848c1d846ff39cf4dbe866641e0a0c75039960b0
alison <adevpr11@gmail.com>
2025-08-20 18:25:33 -0300

    feat: implementa lógica para apresentar lista de movimentações

--- src/app/pages/consultar-financas/consultar-financas.ts
+++ src/app/pages/consultar-financas/consultar-financas.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, signal } from '@angular/core';
 import { Navbar } from '../../shared/navbar/navbar';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import * as CryptoJS from 'crypto-js';
+
 
 @Component({
   selector: 'app-consultar-financas',
-  imports: [Navbar],
+  imports: [
+    Navbar,
+    CommonModule
+  ],
   templateUrl: './consultar-financas.html',
   styleUrl: './consultar-financas.css'
 })
 export class ConsultarFinancas {
 
+  http = inject(HttpClient)
+  changeDetector = inject(ChangeDetectorRef)
+
+  mensagemSucesso = signal('');
+  mensagemErro = signal('');
+  
+  movimentacoes: any[] = [];
+
+  ngOnInit(): void{
+    const usuario = this.getSessionUser();
+    const headers = { Authorization: `Bearer ${usuario.token}` }
+    
+    this.http.get('http://localhost:8084/api/v1/movimentacoes', { headers })
+      .subscribe(
+        {
+          next: (response) => {
+            this.movimentacoes = response as any[];
+            this.changeDetector.detectChanges();
+          },
+          error: (e) => {
+            const mensagem = e.error?.message || 'Erro desconhecido do servidor';
+            this.mensagemErro.set(`Erro ao carregar categorias: ${mensagem}`)
+          }
+        });
+  }
+
+  private getSessionUser(): any {
+    const auth = sessionStorage.getItem('auth');
+    const decrypted = CryptoJS.AES.decrypt(auth as string, 'auth').toString(CryptoJS.enc.Utf8);
+    return JSON.parse(decrypted);
+  }
 }
